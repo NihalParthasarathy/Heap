497daa1993729b1bc4b1790d4aed7ef75149a07e
Bobbert Banana <hanuman.bheema@gmail.com>
2022-03-31 03:27:25 +0100

    finnished file add

--- main.cpp
+++ main.cpp
@@ -27,7 +27,7 @@ int main() {
     char input[10];
     cin >> input;
     if (strcmp(input, "ADD") == 0) {//Calls the add function
-      
+      addFile(array, curr);
     }
     else if (strcmp(input, "TYPE") == 0) {//Calls type function
       manuallyAdd(array, curr);
@@ -48,14 +48,22 @@ int main() {
 void addFile(int* array, int &curr) {
   for (int i = 0; i < 100; i++) {
     char input[10];
+    char tempString[10];
     int count = 1;
+    int numput;
     int randomnum = (rand() % 50) + 1;
     fstream myfile("numberFile.txt");
     while (myfile.getline(input,10, ' ')) {
       if (count == randomnum) {
-	
+	strcpy(tempString, input);
+	count++;
       }
+      count++;
     }
+    numput = atoi(tempString);
+    array[curr] = numput;
+    heap(array, curr);
+    curr++;
   }
 }
 
